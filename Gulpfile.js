fcf765b80f78b9ba72fc2cba2a9b7c3115a6f224
felipebernardes <bernardes.flp@gmail.com>
2016-01-04 23:34:33 -0200

    Adds bower.json

---
+++ Gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+
+gulp.task('dependencies', function() {
+    gulp.src('bower_components/normalize-scss/_normalize.scss')
+        .pipe(gulp.dest('./scss/libs/'));
+});
+
+gulp.task('styles', function() {
+    gulp.src('scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'))
+});
+
+gulp.task('watch',function() {
+    gulp.watch('scss/**/*.scss',['styles']);
+});
+
+gulp.task('sync', function() {
+    browserSync.init({
+        server: ""
+    });
+    gulp.watch("scss/*.scss", ['styles']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("css/*.css").on('change', browserSync.reload);
+});
